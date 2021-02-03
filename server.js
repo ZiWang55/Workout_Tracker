2452200104332c225e670c1db470b16d5bf73f96
zi wang <ziwang55@gmail.com>
2021-02-03 12:17:58 -0600

    updated gitignore

--- server.js
+++ server.js
@@ -7,6 +7,8 @@ const PORT = process.env.PORT || 8080
 
 const app = express()
 
+app.use(morgan('dev'))
+
 app.use(express.urlencoded({extended: true}))
 app.use(express.json())
 app.use(express.static('public'))
@@ -24,3 +26,6 @@ mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/workout_tracker
 require("./routes/apiRoutes")(app);
 require("./routes/htmlRoutes")(app);
 
+app.listen(PORT, function(){
+    console.log('app listening on port ${PORT}')
+})
