a68dd1666e1d0ae6b163457b070bcfd0c9a5816e
zi wang <ziwang55@gmail.com>
2021-02-03 12:10:22 -0600

    added dependencies and routes to serverjs

--- server.js
+++ server.js
@@ -1,2 +1,26 @@
 // Dependencies
-// const express = require 
+const express = require('express')
+const logger = require('morgan')
+const mongoose = require('mongoose')
+
+const PORT = process.env.PORT || 8080
+
+const app = express()
+
+app.use(express.urlencoded({extended: true}))
+app.use(express.json())
+app.use(express.static('public'))
+
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/workout_tracker',
+{
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  useCreateIndex: true,
+  useFindAndModify: false
+}
+);
+
+// Routes
+require("./routes/apiRoutes")(app);
+require("./routes/htmlRoutes")(app);
+
