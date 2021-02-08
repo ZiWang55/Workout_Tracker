d11c8fa4e8f33271719227a0715432d9cad59d9e
zi wang <ziwang55@gmail.com>
2021-02-07 21:30:51 -0600

    editing readme

--- routes/api-routes.js
+++ routes/api-routes.js
@@ -1,53 +1,62 @@
-const db = require('../models')
+const db = require("../models");
 
 module.exports = function (app) {
-    //Get workouts
-    app.get('/api/workouts', (req, res) => {
-        db.Workout.find({}).then(dbWorkout => {
-            dbWorkout.forEach(workout => {
-                var total = 0
-                workout.exercises.forEach(e => {
-                    total += e.duration
-                })
-                workout.totalDuration = total
-            })
-            res.json(dbWorkout)
-        }).catch(err => {
-            res.json(err)
-        })
-    })
-    //Add exercise
-    app.put('/api/workouts/:id', (req, res) => {
-        db.Workout.findOneAndUpdate(
-            {_id: req.params.id},
-            {
-                $inc: { totalDuraction: req.body.duration},
-                $push: {exercises: req.body}
-            },
-            {new:true}).then(dbWorkout => {
-                res.json(dbWorkout)
-            }).catch(err => {
-                res.json(err)
-            })
-    })
+  //Get workouts
+  app.get("/api/workouts", (req, res) => {
+    db.Workout.find({})
+      .then((dbWorkout) => {
+        dbWorkout.forEach((workout) => {
+          var total = 0;
+          workout.exercises.forEach((e) => {
+            total += e.duration;
+          });
+          workout.totalDuration = total;
+        });
+        res.json(dbWorkout);
+      })
+      .catch((err) => {
+        res.json(err);
+      });
+  });
+  //Add exercise
+  app.put("/api/workouts/:id", (req, res) => {
+    db.Workout.findOneAndUpdate(
+      { _id: req.params.id },
+      {
+        $inc: { totalDuraction: req.body.duration },
+        $push: { exercises: req.body },
+      },
+      { new: true }
+    )
+      .then((dbWorkout) => {
+        res.json(dbWorkout);
+      })
+      .catch((err) => {
+        res.json(err);
+      });
+  });
 
-    //Create workout
-    app.post('/api/workouts', ({body}, res)=>{
-        db.Workout.create(body).then((dbWorkout => {
-            res.json(dbWorkout)
-        })).catch(err => {
-            res.json(err)
-        })
-    })
+  //Create workout
+  app.post("/api/workouts", ({ body }, res) => {
+    db.Workout.create(body)
+      .then((dbWorkout) => {
+        res.json(dbWorkout);
+      })
+      .catch((err) => {
+        res.json(err);
+      });
+  });
 
-    //Get workouts
-    app.get('/api/workouts/range', (req, res) => {
-        db.Workout.find({}).then(dbWorkout => {
-            console.log('Here are all the workouts!')
-            console.log(dbWorkout)
-            res.json(dbWorkout)
-        }).catch(err => {
-            res.json(err)
-        })
-    })
-}
+  //Get workouts
+  app.get("/api/workouts/range", (req, res) => {
+    db.Workout.find({})
+      .then((dbWorkout) => {
+        console.log("Here are all the workouts!");
+        console.log(dbWorkout);
+        res.json(dbWorkout);
+      })
+      .catch((err) => {
+        res.json(err);
+      });
+  });
+};
--- server.js
+++ server.js
@@ -1,43 +1,41 @@
 // Dependencies
-const express = require('express')
-const logger = require('morgan')
-const mongoose = require('mongoose')
-const path = require('path')
+const express = require("express");
+const logger = require("morgan");
+const mongoose = require("mongoose");
+const path = require("path");
 
-const PORT = process.env.PORT || 8080
+const PORT = process.env.PORT || 8080;
 
-const app = express()
+const app = express();
 
-app.use(logger('dev'))
+app.use(logger("dev"));
 
-app.use(express.urlencoded({extended: true}))
-app.use(express.json())
-app.use(express.static('public'))
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(express.static("public"));
 
-mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/workout',
-{
-   useNewUrlParser: true,
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workout", {
+  useNewUrlParser: true,
   useUnifiedTopology: true,
   useCreateIndex: true,
-  useFindAndModify: false
-}
-);
+  useFindAndModify: false,
+});
 
-require('./routes/api-routes.js')(app)
+require("./routes/api-routes.js")(app);
 
 // Routes
-app.get('/', (req, res) => {
-    res.sendFile(path.join(_dirname + '/public/index.html'))
-})
+app.get("/", (req, res) => {
+  res.sendFile(path.join(_dirname + "/public/index.html"));
+});
 
 app.get("/exercise", (req, res) => {
-    res.sendFile(path.join(__dirname + "/public/exercise.html"));
+  res.sendFile(path.join(__dirname + "/public/exercise.html"));
 });
 
 app.get("/stats", (req, res) => {
-    res.sendFile(path.join(__dirname + "/public/stats.html"));
+  res.sendFile(path.join(__dirname + "/public/stats.html"));
 });
 
-app.listen(PORT, function(){
-    console.log(`app listening on port ${PORT}`)
-})
+app.listen(PORT, function () {
+  console.log(`app listening on port ${PORT}`);
+});
