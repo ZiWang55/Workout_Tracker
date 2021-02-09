4cfff0378c219bc0f284d437bef41b5a7ca8c195
zi wang <ziwang55@gmail.com>
2021-02-09 14:34:32 -0600

    moving html routes outside of server.js and into the routes folder

--- server.js
+++ server.js
@@ -21,20 +21,10 @@ mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workout", {
   useFindAndModify: false,
 });
 
-require("./routes/api-routes.js")(app);
+require("./routes/api-routes")(app);
+require("./routes/html-routes")(app)
 
-// Routes
-app.get("/", (req, res) => {
-  res.sendFile(path.join(_dirname + "/public/index.html"));
-});
 
-app.get("/exercise", (req, res) => {
-  res.sendFile(path.join(__dirname + "/public/exercise.html"));
-});
-
-app.get("/stats", (req, res) => {
-  res.sendFile(path.join(__dirname + "/public/stats.html"));
-});
 
 app.listen(PORT, function () {
   console.log(`app listening on port ${PORT}`);
